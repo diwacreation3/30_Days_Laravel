b101946f37f7035485a6e375662c478392cd732d
Diwakar <diwakarcreation@gmail.com>
2024-12-01 19:25:12 +0545

    Learned: Routes 🍁

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('hello');
+});
+
+Route::get('/about', function()
+{
+return view ('about');
 });

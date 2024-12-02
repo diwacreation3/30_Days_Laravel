c7b3bba1480c9b777829596b70d79b1bd56e5fc5
Diwakar <diwakarcreation@gmail.com>
2024-12-02 19:44:39 +0545

    Learned : Pretty layout with tailwind CSS 🍁

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+<x-slot:heading>
+        About Page
+    </x-slot:heading>
     <h1>Hello i am aboutpage </h1>
 </x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,7 @@
 <x-layout>
+    <x-slot:heading>
+        Home Page
+    </x-slot:heading>
+
     <h1>Hello i am homepage </h1>
 </x-layout>
